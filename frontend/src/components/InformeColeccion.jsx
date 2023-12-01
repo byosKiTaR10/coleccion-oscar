08ec4df9b2ce8f09ddc2e2f449d468c36cd3e7ae
byosKiTaR10 <os-kita@hotmail.es>
2023-12-01 00:57:53 +0000

    InformesDone

--- frontend/src/components/InformeColeccion.jsx
+++ frontend/src/components/InformeColeccion.jsx
@@ -1,48 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import MaterialTable from "@material-table/core";
 import { ExportCsv, ExportPdf } from "@material-table/exporters";
 const InformeColeccion = (props) => {
-    const [tableData, setTableData] = useState([]);
     const col = [
         { title: "Nombre", field: "nombre" },
         { title: "Marca", field: "marca" },
         { title: "Tipo", field: "tipo" },
         { title: "Precio", field: "precio"}
     ]
-    const handleTabla = async () => {
-        try {
-            fetch('http://localhost:3030/getItems')
-                .then(response => {
-                    // Verificar si la solicitud fue exitosa (código de estado 200-299)
-                    if (!response.ok) {
-                        throw new Error(`Error de red: ${response.status}`);
-                    }
-                    // Parsear la respuesta como JSON
-                    return response.json();
-                })
-                .then(data => {
-                    // Manejar los datos obtenidos
-                    setTableData(data.data)
-                    // Aquí puedes realizar cualquier operación que necesites con los datos
-                })
-                .catch(error => {
-                    // Manejar errores de red o errores en la respuesta
-                    console.error('Error en la solicitud:', error);
-                });
-        } catch (err) {
-            console.error("Error obteniendo datos de la tabla")
-        }
-    }
-
-    // Efecto para verificar la autenticación al cargar la página
-    useEffect(() => {
-        handleTabla()
-    }, [])
-
     return (
         <MaterialTable
             title="Informe"
-            columns={col} data={tableData}
+            columns={col} data={props.tableData}
             //Esto de aquí abajo es lo nuevo. Dentro de options pondremos la parte de exportación
             options={{
                 draggable: false,
--- frontend/src/components/Informes.jsx
+++ frontend/src/components/Informes.jsx
@@ -1,13 +1,43 @@
 import Topbar from "./Topbar";
 import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
 import { Grid } from "@mui/material";
-import { useState } from "react";
 import InformeColeccion from "./InformeColeccion";
 const Informes = () => {
+    const [tableData, setTableData] = useState([]);
     const [mostrarInforme, setMostrarInforme] = useState(false);
     const handleMostrarInforme = () => {
         setMostrarInforme(true);
     };
+    const handleTabla = async () => {
+        try {
+            fetch('http://localhost:3030/getItems')
+                .then(response => {
+                    // Verificar si la solicitud fue exitosa (código de estado 200-299)
+                    if (!response.ok) {
+                        throw new Error(`Error de red: ${response.status}`);
+                    }
+                    // Parsear la respuesta como JSON
+                    return response.json();
+                })
+                .then(data => {
+                    // Manejar los datos obtenidos
+                    setTableData(data.data)
+                    // Aquí puedes realizar cualquier operación que necesites con los datos
+                })
+                .catch(error => {
+                    // Manejar errores de red o errores en la respuesta
+                    console.error('Error en la solicitud:', error);
+                });
+        } catch (err) {
+            console.error("Error obteniendo datos de la tabla")
+        }
+    }
+
+    // Efecto para verificar la autenticación al cargar la página
+    useEffect(() => {
+        handleTabla()
+    }, [])
 
     return (
         <div>
@@ -23,7 +53,7 @@ const Informes = () => {
                     </Button>
                 </Grid>:
                  <div>
-                    <InformeColeccion />
+                    <InformeColeccion tableData={tableData}/>
                 </div>
             }
         </div>
